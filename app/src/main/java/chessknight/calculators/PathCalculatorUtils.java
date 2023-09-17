0beae50452479e3d18ee59f82f4d7f64775cb65d
rafalzonk <rafalzonk@gmail.com>
2023-09-17 20:27:30 +0200

    Checking whether coordinates are within given ring with tests

--- app/src/main/java/chessknight/calculators/PathCalculatorUtils.java
+++ app/src/main/java/chessknight/calculators/PathCalculatorUtils.java
@@ -3,7 +3,21 @@ package chessknight.calculators;
 class PathCalculatorUtils {
 
     static KnightPosition goEast(int[][] board, int step, int startX, int startY, int ringLevel) {
-        return null;
+        var x = startX;
+        var y = startY;
+
+        var tmpX = x;
+        var tmpY = y;
+
+        do {
+            board[x][y] = step++;
+            tmpX = x;
+            tmpY = y;
+            x += (x % 2 == 0 ? 1 : -1);
+            y += 2;
+        } while (withinRing(board, x, y, ringLevel));
+
+        return new KnightPosition(tmpX, tmpY, step);
     }
 
     static KnightPosition goSouth(int[][] board, int step, int startX, int startY, int ringLevel) {
@@ -16,5 +30,19 @@ class PathCalculatorUtils {
 
     static KnightPosition goNorth(int[][] board, int step, int startX, int startY, int ringLevel) {
         return null;
+    }
+
+    static boolean withinRing(int[][] board, int x, int y, int ringLevel) {
+        return !withinInnerRing(board, x, y, ringLevel) && !withinOuterRing(board, x, y, ringLevel);
+    }
+
+    static boolean withinOuterRing(int[][] board, int x, int y, int ringLevel) {
+        return ringLevel < 0 ||
+                !withinInnerRing(board, x, y, ringLevel - 1);
+    }
+
+    static boolean withinInnerRing(int[][] board, int x, int y, int ringLevel) {
+        return x >= (1 + ringLevel) * 2 && x < (board.length - (1 + ringLevel) * 2) &&
+                y >= (1 + ringLevel) * 2 && y < (board[0].length - (1 + ringLevel) * 2);
     }
 }
--- app/src/test/groovy/chessknight/calculators/PathCalculatorUtilsTestData.groovy
+++ app/src/test/groovy/chessknight/calculators/PathCalculatorUtilsTestData.groovy
@@ -2,4 +2,7 @@ package chessknight.calculators
 
 trait PathCalculatorUtilsTestData {
 
+    def int[][] matrix10x10() {
+        new int[10][10]
+    }
 }
